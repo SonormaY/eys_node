e99e99e21246128c4cb966da905c4ccb8a443330
Stanislav Humeniuk <stanislav.humeniuk@outlook.com>
2024-11-12 18:49:09 +0200

    update index_rpi

--- server/index_rpi.js
+++ server/index_rpi.js
@@ -25,6 +25,11 @@ function ensureSecure(req, res, next) {
 
 app.use(ensureSecure);
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/auth', authRoutes);
 app.use(cors({
   origin: 'https://rpi4.uno',
