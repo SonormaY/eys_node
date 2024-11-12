965c3eb609b896cd06d0028456e7a6d2bfe3a2a9
Stanislav Humeniuk <stanislav.humeniuk@outlook.com>
2024-11-12 17:51:19 +0200

    update build_run.sh

--- build_run.sh
+++ build_run.sh
@@ -8,7 +8,10 @@ sudo cp -r dist /var/www/client
 
 # 3. run server in tmux session
 cd ../server
-if tmux ls | grep -q "^server:"; then
-    tmux kill-session -t server
+if tmux info &>/dev/null; then
+    # Check if a tmux session named 'server' exists
+    if tmux ls | grep -q "^server:"; then
+        tmux kill-session -t server
+    fi
 fi
 tmux new-session -d -s server 'npm run deploy'
